85b613f7377556686e2868feaa8737d38d832a02
DEVENDRA SOLANKI <devendrasolanki8804@gmail.com>
2025-03-11 22:34:12 +0530

    Deven Bhai

---
+++ 01_Basics/04_test.js
@@ -0,0 +1,20 @@
+// let score = "33dev"
+
+// console.log(typeof score);
+
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber);
+
+let isloggedin = 1
+
+console.log(isloggedin);
+console.log(typeof isloggedin);
+
+
+let booleanisLoggedin = Boolean(isloggedin)
+console.log(typeof booleanisLoggedin)
+console.log(booleanisLoggedin)
+
+
+
